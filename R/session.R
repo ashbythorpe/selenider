83a887e488c92c33e9d9b163f77ce803e0ca11ef
Ashby Thorpe <ashbythorpe@gmail.com>
2023-05-06 21:33:38 +0100

    Improve storage method for local session

--- R/session.R
+++ R/session.R
@@ -6,16 +6,71 @@
 #' @param browser The name of the browser to run the session in.
 #' @param timeout The default time to wait when collecting an element.
 #' @param driver A driver object to use instead of creating one manually.
-#' @param global Whether to set the session as the global session object.
+#' @param local Whether to set the session as the local session object, 
+#'   using [local_session()].
+#' @param .env Passed into [local_session()] function, to define the 
+#'   environment in which the session is used. Change this if you want to
+#'   create the session inside a function and then use it outside the
+#'   function.
 #'
 #' @details
 #' `selenider_session()` uses [RSelenium::rsDriver()] to create a browser
-#' session.
+#' session. It then uses withr to set the session locally (unless otherwise
+#' specified). To reset the local session object, use [withr::deferred_run()].
 #'
-#' @seealso [close_session()]
+#' @seealso 
+#' * [close_session()] to close the session. Note that this will not reset the 
+#'   result of [get_session()], which is why [withr::deferred_run()] is preferred.
+#' * [local_session()] and [with_session()] to manually set the local session
+#'   object.
 #'
 #' @returns
 #' A `selenider_session` object.
+#'
+#' @examples
+#' \dontrun{
+#' # Note that these examples will create real browser sessions, so should only be
+#' # run interactively.
+#'
+#' session <- selenider_session(browser = "firefox")
+#'
+#' withr::deferred_run() # Reset the session
+#'
+#' # If we want to use the session manually:
+#' selenider_session(local = FALSE)
+#'
+#' session_1 <- selenider_session(timeout = 10)
+#' # session_1 is the local session here
+#'
+#' my_function <- function() {
+#'   session <- selenider_session(browser = "internet explorer")
+#'
+#'   # In here, session_2 is the local session
+#'   get_session()
+#' } # When the function finishes executing, the session is closed
+#'
+#' my_function() # Returns `session_2`
+#'
+#' # But outside the function, session_1 is the global session again
+#'
+#' get_session() # Returns `session_1`
+#'  
+#' withr::deferred_run() # Close `session_1`
+#'
+#' # If we want to use a session outside the scope of a function,
+#' # we need to use the `.env` argument.
+#' create_session <- function(timeout = 10, .env = rlang::caller_env()) {
+#'   # caller_env() is the environment where the function is called
+#'   selenider_session(browser = "firefox", timeout = timeout, .env = .env)
+#' }
+#'
+#' my_session <- create_session()
+#'
+#' # We can now use this session outside the `create_session()` function
+#' get_session()
+#'
+#' withr::deferred_run() # Close `my_session`
+#' }
 #'
 #' @export
 selenider_session <- function(browser = c(
@@ -24,10 +79,12 @@ selenider_session <- function(browser = c(
                               ),
                               timeout = 4,
                               driver = NULL,
-                              global = TRUE) {
+                              local = TRUE,
+                              quiet = TRUE,
+                              .env = rlang::caller_env()) {
   # Allow e.g. 'Firefox'
-  browser <- rlang::arg_match(
-    browser,
+  browser <- rlang::arg_match0(
+    browser[1],
     c(browser, tools::toTitleCase(browser), "PhantomJS")
   )
 
@@ -37,19 +94,31 @@ selenider_session <- function(browser = c(
   phantomver <- if (browser == "phantomjs") "latest" else NULL
 
   if (is.null(driver)) {
-    driver <- RSelenium::rsDriver(
-      browser = browser,
-      chromever = chromever,
-      geckover = geckover,
-      iedrver = iedrver,
-      phantomver = phantomver
-    )
+    if (quiet) {
+      capture.output({
+        driver <- suppressMessages(RSelenium::rsDriver(
+          browser = browser,
+          chromever = chromever,
+          geckover = geckover,
+          iedrver = iedrver,
+          phantomver = phantomver
+        ))
+      })
+    } else {
+      driver <- RSelenium::rsDriver(
+        browser = browser,
+        chromever = chromever,
+        geckover = geckover,
+        iedrver = iedrver,
+        phantomver = phantomver
+      )
+    }
   }
   
   session <- new_selenider_session(driver, timeout)
   
-  if (global) {
-    set_session(session)
+  if (local) {
+    local_session(session, .local_envir = .env)
   }
   
   session
@@ -75,7 +144,10 @@ new_selenider_session <- function(driver, timeout) {
 #'
 #' @param x A `selenider_session` object. If omitted, the global session object
 #'   will be closed.
-#'
+#' 
+#' @returns 
+#' Nothing.
+#' 
 #' @examples
 #' session <- mock_selenider_session()
 #'
@@ -90,8 +162,15 @@ close_session <- function(x = NULL) {
     x <- get_session()
   }
   
-  x$client$close()
-  x$server$stop()
+  rlang::try_fetch(
+    x$driver$client$close(),
+    error = function(e) {
+      x$driver$server$stop()
+      cli::cli_abort("Could not close session", parent = e)
+    }
+  )
+  
+  invisible(x$driver$server$stop())
 }
 
 #' @export
