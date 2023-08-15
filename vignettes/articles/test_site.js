e330b9ea10dba5f98678a8617fa3f530244ebe04
Ashby Thorpe <ashbythorpe@gmail.com>
2023-08-15 12:27:27 +0100

    Updated website

--- vignettes/articles/test_site.js
+++ vignettes/articles/test_site.js
@@ -4,4 +4,25 @@ $(function() {
   $("#toggle_div").click(function() {
     $(".toggleable").toggle();
   })
+
+  $(".actions-button").on("mousedown", function(e) {
+    const text = e.which === 1 ? "Left clicked" : "Right clicked";
+    $("#button-output").text(text);
+  })
+
+  $(".actions-button").on("dblclick", function() {
+    $("#button-output").text("Double clicked!");
+  })
+
+  $(".actions-button").hover(function() {
+    $("#button-output").text("Hovered!");
+  })
+
+  $(".actions-input").on("input", function() {
+    $("#text-output").text($(this).val());
+  })
+
+  $(".actions-form").on("submit", function(e) {
+    $("#form-output").text("Form submitted!");
+  })
 })
