4e112b6b3598bb2643aa580236efbb85434977c3
Ashby Thorpe <ashbythorpe@gmail.com>
2023-08-19 18:14:36 +0100

    Try switching modifiers again

--- R/actions.R
+++ R/actions.R
@@ -363,7 +363,8 @@ set_value <- function(x, text, timeout = NULL) {
 chromote_clear <- function(x, driver) {
   click_chromote(x, driver = driver)
 
-  chromote_press(driver, modifiers = 2, key = "a", code = "KeyA", windowsVirtualKeyCode = 65)
+  modifiers <- if (is_mac()) 4 else 2
+  chromote_press(driver, modifiers = modifiers, key = "a", code = "KeyA", windowsVirtualKeyCode = 65)
 
   chromote_press(driver, windowsVirtualKeyCode = 8, code = "Backspace", key = "Backspace") 
 }
