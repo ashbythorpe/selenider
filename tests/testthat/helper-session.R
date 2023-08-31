5f80c2c2f14a6948397b1599c0d8ca0ac1bdbb4a
Ashby Thorpe <ashbythorpe@gmail.com>
2023-08-31 12:23:17 +0100

    Add Github Action for Selenium

--- tests/testthat/helper-session.R
+++ tests/testthat/helper-session.R
@@ -1,6 +1,8 @@
 selenider_test_session <- function(x, .env = rlang::caller_env()) {
   session <- Sys.getenv("SELENIDER_SESSION", "chromote")
   browser <- Sys.getenv("SELENIDER_BROWSER", "chrome")
+  ip <- Sys.getenv("SELENIDER_IP")
+  port <- as.integer(Sys.getenv("SELENIDER_PORT", "4567"))
 
   if (session == "chromote") {
     chromote::set_chrome_args(c(
@@ -16,6 +18,9 @@ selenider_test_session <- function(x, .env = rlang::caller_env()) {
       # Delete the Crashpad folder if it exists
       unlink(file.path(tempdir(), "Crashpad"), recursive = TRUE)
     }, envir = .env)
+  } else if (ip != "") {
+    client <- create_selenium_client(browser, port = port, remoteServerAddr = ip)
+    result <- selenider_session(driver = client)
   } else {
     result <- selenider_session(session, browser = browser, .env = .env)
   }
