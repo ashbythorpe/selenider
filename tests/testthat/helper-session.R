b12bd3d1401497d5caeb8151a3edc98559bd8eaf
Ashby Thorpe <ashbythorpe@gmail.com>
2023-08-31 12:35:04 +0100

    Use host.docker.internal as IP

--- tests/testthat/helper-session.R
+++ tests/testthat/helper-session.R
@@ -1,7 +1,7 @@
 selenider_test_session <- function(x, .env = rlang::caller_env()) {
   session <- Sys.getenv("SELENIDER_SESSION", "chromote")
   browser <- Sys.getenv("SELENIDER_BROWSER", "chrome")
-  ip <- Sys.getenv("SELENIDER_IP")
+  ip <- as.logical(Sys.getenv("SELENIDER_DOCKER", "FALSE"))
   port <- as.integer(Sys.getenv("SELENIDER_PORT", "4567"))
 
   if (session == "chromote") {
@@ -18,8 +18,8 @@ selenider_test_session <- function(x, .env = rlang::caller_env()) {
       # Delete the Crashpad folder if it exists
       unlink(file.path(tempdir(), "Crashpad"), recursive = TRUE)
     }, envir = .env)
-  } else if (ip != "") {
-    client <- create_selenium_client(browser, port = port, remoteServerAddr = ip)
+  } else if (docker) {
+    client <- create_selenium_client(browser, port = port, remoteServerAddr = "host.docker.internal")
     result <- selenider_session(driver = client)
   } else {
     result <- selenider_session(session, browser = browser, .env = .env)
