3a8c2f8c8ddcbe475d79f7d69adbdd47d6177b51
ashbythorpe <61390103+ashbythorpe@users.noreply.github.com>
2023-11-02 19:04:31 +0000

    Deploying to gh-pages from @ ashbythorpe/selenider@30bbc8c192c4a90870de3f097bdac2c70bc46467 🚀

---
+++ dev/articles/test_site.js
@@ -0,0 +1,29 @@
+$(function() {
+  $(".toggleable").hide();
+
+  $("#toggle_div").click(function() {
+    $(".toggleable").toggle();
+  })
+
+  $(".actions-button").on("mousedown", function(e) {
+    const text = e.which === 1 ? "Left clicked" : "Right clicked";
+    $("#button-output").text(text);
+  })
+
+  $(".actions-button").on("dblclick", function() {
+    $("#button-output").text("Double clicked!");
+  })
+
+  $(".actions-button").hover(function() {
+    $("#button-output").text("Hovered!");
+  })
+
+  $(".actions-input").on("input", function() {
+    $("#text-output").text($(this).val());
+  })
+
+  $(".actions-form").on("submit", function(e) {
+    e.preventDefault();
+    $("#form-output").text("Form submitted!");
+  })
+})
