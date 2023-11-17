06ff22a211902debb6f3952e37a5dc4bc5f9a5d1
ashbythorpe <61390103+ashbythorpe@users.noreply.github.com>
2023-11-17 00:17:01 +0000

    Deploying to gh-pages from @ ashbythorpe/selenider@c701b72b8c9f7d26f08b3621505af2fc4aec8ba3 🚀

--- dev/articles/test_site.js
+++ dev/articles/test_site.js
@@ -1,29 +1,41 @@
-$(function() {
+$(function () {
   $(".toggleable").hide();
 
-  $("#toggle_div").click(function() {
+  $("#toggle_div").click(function () {
     $(".toggleable").toggle();
-  })
+  });
 
-  $(".actions-button").on("mousedown", function(e) {
+  $(".actions-button").on("mousedown", function (e) {
     const text = e.which === 1 ? "Left clicked" : "Right clicked";
     $("#button-output").text(text);
-  })
+  });
 
-  $(".actions-button").on("dblclick", function() {
+  $(".actions-button").on("dblclick", function () {
     $("#button-output").text("Double clicked!");
-  })
+  });
 
-  $(".actions-button").hover(function() {
+  $(".actions-button").hover(function () {
     $("#button-output").text("Hovered!");
-  })
+  });
 
-  $(".actions-input").on("input", function() {
+  $(".actions-input").on("input", function () {
     $("#text-output").text($(this).val());
-  })
+  });
 
-  $(".actions-form").on("submit", function(e) {
+  $(".actions-form").on("submit", function (e) {
     e.preventDefault();
     $("#form-output").text("Form submitted!");
-  })
-})
+  });
+
+  $("#selection").on("change", function () {
+    $("#selection-output").text($(this).val());
+  });
+
+  $("#multiple-selection").on("change", function () {
+    $("#multiple-selection-output").text(
+      $(this)
+        .val()
+        .reduce((x, y) => x + y, 0),
+    );
+  });
+});
