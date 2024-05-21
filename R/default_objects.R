80ba4d2251dd2ea501c75ba9a5cfe4f9f89d9e71
Ashby Thorpe <ashbythorpe@gmail.com>
2024-05-21 22:14:41 +0100

    Change the way we manage the default chromote object

--- R/default_objects.R
+++ R/default_objects.R
@@ -23,10 +23,25 @@ set_default_selenium_options <- function(x) {
   set_in_env(selenium_options = x)
 }
 
-default_chromote_args <- function() {
+# We manage our own default chromote object to make sure we make a new one
+# whenever the arguments passed into chrome change.
+default_chromote_object <- function() {
   get_from_env("chromote")
 }
 
-set_default_chromote_args <- function(x) {
+has_default_chromote_object <- function() {
+  !is.null(default_chromote_object()) &&
+    default_chromote_object()$get_browser()$get_process()$is_alive()
+}
+
+set_default_chromote_object <- function(x) {
   set_in_env(chromote = x)
+}
+
+default_chromote_args <- function() {
+  get_from_env("chromote_args")
+}
+
+set_default_chromote_args <- function(x) {
+  set_in_env(chromote_args = x)
 }
